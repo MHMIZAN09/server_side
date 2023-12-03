232706c709d0a9c799784a33e318157e2901fdce
MHMIZAN09 <mhmizan.cse.gub@gmail.com>
2023-12-03 12:26:57 +0600

    Add cookie-parser dependency and import it in app.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import dotenv from "dotenv";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path:'./env'
+    path: './env'
 });
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    })
+    .catch(error => console.log(error, "Mongodb connection error"));
 
